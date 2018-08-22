c47ec20de04d076a8594b1b81e9dc9334f4a3a9a
Junior743 <carloshenriquesousajunior@gmail.com>
2018-08-22 14:21:22 -0300

    Correction | namespaces and decorators

--- controllers.py
+++ controllers.py
@@ -1,24 +1,27 @@
-from batpay.oath import OAuth
-from batpay.utils import ConsumidorREST
+from oauth2 import OAuth2
+from utils import RESTConsumer
+from oauth2 import authenticate
 
 
 class ControllerAuthorization(object):
 
     #region Built-in
     def __call__(self):
-        OAuth.authorize()
+        OAuth2.authorize()
     #endregion
 
 class ControllerExcel(object):
 
     #region Metodos Publicos
-    def distribuir_contas(self):
+    @staticmethod
+    def distribuir_contas(sessao):
         pass
     #endregion
 
 class ControllerPDF(object):
 
     #region Metodos Publicos
-    def distribuir_contas(self):
+    @staticmethod
+    def distribuir_contas(sessao):
         pass
     #endregion
